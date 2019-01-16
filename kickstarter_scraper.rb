844402dcc009f53df67b8c93f7b482ae03672c68
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-16 01:56:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,10 @@ def create_project_hash
     html = File.read('fixtures/kickstarter.html')
  
     kickstarter = Nokogiri::HTML(html)
+    kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
   
+  projects
   
 end
